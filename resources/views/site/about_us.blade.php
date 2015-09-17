013311abc7c7daf8b9fa4e5e5e3326e44834fe12
Mutlu-Erdem <mutluerdem7@gmail.com>
2015-09-17 04:51:57 +0300

    default.css minified.

--- resources/views/site/about_us.blade.php
+++ resources/views/site/about_us.blade.php
@@ -3,8 +3,9 @@
 @section("title", trans("menu.about_us"))
 
 @section("content")
+<div class="container">
     <div class="row">
-        <div class="col-md-6 col-md-offset-3">
+        <div class="col-md-12">
             <div class="row">
                 <div class="col-md-12 about-giris">
                     <div class="col-md-4">
@@ -52,6 +53,7 @@
             </div>
         </div>
     </div>
+</div>
 @endsection
 
 @section("styles")
