f491afdd10ddf37d684007836bc95db6bbbf3733
ozanmuyes <ozi5169@gmail.com>
2015-10-19 00:57:01 +0300

    New slide photo added

--- resources/views/partials/site/slider.blade.php
+++ resources/views/partials/site/slider.blade.php
@@ -1,17 +1,17 @@
 <section>
     <div data-ride="carousel" class="carousel slide" id="carousel-example-generic">
         <ol class="carousel-indicators">
-            <li class="" data-slide-to="0" data-target="#carousel-example-generic"></li>
-            <li data-slide-to="1" data-target="#carousel-example-generic" class="active"></li>
+            <li data-slide-to="0" data-target="#carousel-example-generic" class="active"></li>
+            <li data-slide-to="1" data-target="#carousel-example-generic"></li>
             <li data-slide-to="2" data-target="#carousel-example-generic"></li>
             <li data-slide-to="3" data-target="#carousel-example-generic"></li>
         </ol>
 
         <div role="listbox" class="carousel-inner">
-            <div class="item">
-                <img alt="First slide" src="img/slide/oscar.jpg">
-            </div>
 
+            <div class="item">
+                <img alt="First slide" src="img/slide/first.jpg">
+            </div>
             <div class="item active">
                 <img alt="Second slide" src="img/slide/sans-oil-paint.jpg">
             </div>
--- resources/views/site/guide.blade.php
+++ resources/views/site/guide.blade.php
@@ -3,141 +3,86 @@
 @section("title", "Rehber")
 
 @section("content")
-
-<div class="container">
-
+    <div class="container">
         <div class="row">
             <div class="col-md-12 guide-title">
                 <h1 class="guide-text">FOTOĞRAFINIZI SEÇERKEN...</h1>
             </div>
         </div>
 
-      <div class="row">
-        <div class="col-md-12 guiderow">
-          <div id="secondSlider">
-            <ul class="slides">
-              <li>
+        <div class="row">
+            <div class="col-md-12 guiderow">
+                <div id="secondSlider">
+                    <ul class="slides">
+                        <li>
+                            <div class="col-md-4">
+                                <div class="flat-box">
+                                    <div class="colourway colour1"></div>
+                                    <p class="title">Doğru Örnek</p>
+                                    <p class="feature-text">fotoğrafın neden basarılı olduğuna dair açıklama yapılabilecek alan.</p>
+                                </div>
+                            </div>
 
-                <div class="col-md-4">
-                  <div class="flat-box">
-                    <div class="colourway colour1"></div>
-                    <p class="title">Doğru Örnek</p>
-                    <p class="feature-text">fotoğrafın neden basarılı olduğuna dair açıklama yapılabilecek alan.</p>
-                  </div>
+                            <div class="col-md-4">
+                                <div class="flat-box">
+                                    <div class="colourway colour2"></div>
+                                    <p class="title">Kötü Örnek</p>
+                                    <p class="feature-text">fotoğrafın neden basarısız olduğuna dair açıklama yapılabilecek alan.</p>
+                                </div>
+                            </div>
+
+
+                            <div class="col-md-4 design-content">
+                                <h1>Yeterli ışık</h1>
+                                <p>Isıklandırma hakkında gerekli bilgiler girilebilecek bir açıklama alanı. Detaylı bilgiler de girilebilecek bir açıklama alanı. Fotoğraf açısı hakkında gerekli bilgiler girilebilecek bir açıklama alanı. Detaylı bilgiler de girilebilecek bir açıklama alanı</p>
+
+                                {{-- <div class="text-right controlsud">
+                                    <a href="prev" class="prev"><i class="fa fa-angle-left fa-3x"></i></a>
+                                    <a href="next" class="next"><i class="fa fa-angle-right fa-3x"></i></a>
+                                </div> --}}
+                            </div>
+                        </li>
+                    </ul>
                 </div>
-
-                <div class="col-md-4">
-                    <div class="flat-box">
-                    <div class="colourway colour2"></div>
-                    <p class="title">Kötü Örnek</p>
-                    <p class="feature-text">fotoğrafın neden basarısız olduğuna dair açıklama yapılabilecek alan.</p>
-                    </div>
-                </div>
-
-
-                <div class="col-md-4 design-content">
-                    <h1>Yeterli ışık</h1>
-                    <p>Isıklandırma hakkında gerekli bilgiler
-                    girilebilecek bir açıklama alanı. Detaylı bilgiler de girilebilecek bir açıklama alanı. Fotoğraf açısı hakkında gerekli bilgiler
-                    girilebilecek bir açıklama alanı. Detaylı bilgiler de girilebilecek bir açıklama alanı</p>
-                    <br>
-
-                    <div class="text-right controlsud">
-                        <a href="prev" class="prev"><i class="fa fa-angle-left fa-3x"></i></a>
-                        <a href="next" class="next"><i class="fa fa-angle-right fa-3x"></i></a>
-                    </div>
-                </div>
-
-              </li>
-            </ul>
-          </div>
+            </div>
         </div>
-      </div>
 
+        <div class="row">
+            <div class="col-md-12 guiderow">
+                <div id="secondSlider">
+                    <ul class="slides">
+                        <li>
+                            <div class="col-md-4 design-content">
+                                <h1>Bir miktar çözünürlük</h1>
+                                <p>Çözünürlük hakkında gerekli bilgiler girilebilecek bir açıklama alanı. Detaylı bilgiler de girilebilecek bir açıklama alanı. Fotoğraf açısı hakkında gerekli bilgiler girilebilecek bir açıklama alanı. Detaylı bilgiler de girilebilecek bir açıklama alanı.</p>
 
+                                {{-- <div class="text-right controls-medium">
+                                    <a href="prev" class="prev"><i class="fa fa-angle-left fa-3x"></i></a>
+                                    <a href="next" class="next"><i class="fa fa-angle-right fa-3x"></i></a>
+                                </div> --}}
+                            </div>
 
-    <div class="row">
-        <div class="col-md-12 guiderow">
-          <div id="secondSlider">
-            <ul class="slides">
-              <li>
+                            <div class="col-md-4">
+                                <div class="flat-box">
+                                    <div class="colourway colour1"></div>
+                                    <p class="title">Doğru Örnek</p>
+                                    <p class="feature-text">fotoğrafın neden basarılı olduğuna dair açıklama yapılabilecek alan.</p>
+                                </div>
+                            </div>
 
-                <div class="col-md-4 design-content">
-                  <h1>Bir miktar çözünürlük</h1>
-                  <p>Çözünürlük hakkında gerekli bilgiler
-                    girilebilecek bir açıklama alanı. Detaylı bilgiler de girilebilecek bir açıklama alanı. Fotoğraf açısı hakkında gerekli bilgiler
-                    girilebilecek bir açıklama alanı. Detaylı bilgiler de girilebilecek bir açıklama alanı.</p>
-                    <div class="text-right controls-medium">
-                        <a href="prev" class="prev"><i class="fa fa-angle-left fa-3x"></i></a>
-                        <a href="next" class="next"><i class="fa fa-angle-right fa-3x"></i></a>
-                    </div>
+                            <div class="col-md-4">
+                                <div class="flat-box">
+                                    <div class="colourway colour2"></div>
+                                    <p class="title">Kötü Örnek</p>
+                                    <p class="feature-text">fotoğrafın neden basarısız olduğuna dair açıklama yapılabilecek alan.</p>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
-
-                <div class="col-md-4">
-                  <div class="flat-box">
-                    <div class="colourway colour1"></div>
-                    <p class="title">Doğru Örnek</p>
-                    <p class="feature-text">fotoğrafın neden basarılı olduğuna dair açıklama yapılabilecek alan.</p>
-                  </div>
-                </div>
-
-                <div class="col-md-4">
-                  <div class="flat-box">
-                    <div class="colourway colour2"></div>
-                    <p class="title">Kötü Örnek</p>
-                    <p class="feature-text">fotoğrafın neden basarısız olduğuna dair açıklama yapılabilecek alan.</p>
-                  </div>
-                </div>
-
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-
-
-    <div class="row">
-        <div class="col-md-12 guiderow">
-          <div id="secondSlider">
-            <ul class="slides">
-              <li>
-
-                <div class="col-md-4">
-                  <div class="flat-box">
-                    <div class="colourway colour1"></div>
-                    <p class="title">Doğru Örnek</p>
-                    <p class="feature-text">fotoğrafın neden basarılı olduğuna dair açıklama yapılabilecek alan.</p>
-                  </div>
-                </div>
-
-                <div class="col-md-4">
-                  <div class="flat-box">
-                    <div class="colourway colour2"></div>
-                    <p class="title">Kötü Örnek</p>
-                    <p class="feature-text">fotoğrafın neden basarısız olduğuna dair açıklama yapılabilecek alan.</p>
-                  </div>
-                </div>
-
-                <div class="col-md-4 design-content">
-                    <h1>Doğru Açı</h1>
-                    <p>Fotoğraf açısı hakkında gerekli bilgiler
-                    girilebilecek bir açıklama alanı. Detaylı bilgiler de girilebilecek bir açıklama alanı. Fotoğraf açısı hakkında gerekli bilgiler
-                    girilebilecek bir açıklama alanı. Detaylı bilgiler de girilebilecek bir açıklama alanı...</p>
-                    <br>
-
-                    <div class="text-right controlsud">
-                    <a href="prev" class="prev"><i class="fa fa-angle-left fa-3x"></i></a>
-                    <a href="next" class="next"><i class="fa fa-angle-right fa-3x"></i></a>
-                    </div>
-                </div>
-
-              </li>
-            </ul>
-          </div>
+            </div>
         </div>
     </div>
-
-</div>
 @endsection
 
 @section("styles")
